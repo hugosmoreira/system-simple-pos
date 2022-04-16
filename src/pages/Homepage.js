a5c5b38fd80af31a3948dca51e83525cf4f36f1b
hugosmoreira <hugo.n4@gmail.com>
2022-04-16 01:18:56 -0700

    packages install, antd layout boiler

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
+
+
+const Homepage = () => {
+  return (
+    <DefaultLayout>
+        <h1>Home</h1>
+    </DefaultLayout>
+  )
+}
+
+export default Homepage
