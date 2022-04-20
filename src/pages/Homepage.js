3e6afa4eb4901acd905cac9ddd896b1a67cd42e2
hugosmoreira <hugo.n4@gmail.com>
2022-04-19 21:06:11 -0700

    update

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DefaultLayout from '../components/DefaultLayout'
+import axios from 'axios'
+
 
 
 const Homepage = () => {
+
+  const getAllItems = () => {
+    axios.get('/api/items/get-all-items')
+    .then((response) => {
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    getAllItems()
+  }, [])
+    
+
   return (
     <DefaultLayout>
         
